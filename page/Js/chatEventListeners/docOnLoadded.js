c7de26a9a1d2bca5b1332fd7b7e16ac82d31fb31
Arda-f <96407570+Arda-f@users.noreply.github.com>
2022-11-22 14:09:56 +0300

    Update docOnLoadded.js

--- page/Js/chatEventListeners/docOnLoadded.js
+++ page/Js/chatEventListeners/docOnLoadded.js
@@ -1,10 +1,5 @@
-function execute(socket, item, dt, btn, out) {
-    document.addEventListener("DOMContentLoaded", () => {
-        socket.emit("history", {message: "", users: ""})
-        if(item.user != ""){
-            socket.emit("auth", {user:item.user, pass:item.pass}) 
-        }
-    })
-}
+//function execute(socket, item, dt, btn, out) {
+    
+//}
 
-export default execute
+//export default execute
