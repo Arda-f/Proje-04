49f6b1787f2991b4338e85b7ef6350b0fd8ec567
Arda-f <96407570+Arda-f@users.noreply.github.com>
2022-11-26 21:53:29 +0300

    4/1/1

--- events/deleteMessageBox.js
+++ events/deleteMessageBox.js
@@ -4,13 +4,12 @@ module.exports = {
         socket.on("deleteMessageBox", datas => {
             client.db.all(`SELECT * FROM ${datas.sender}sMessageBoxs `, (err, rows) => {
                 rows.forEach(row => {           
-                    console.log(row)
                     if(row.name == datas.name)
                     {
                         var liteRun = `DROP TABLE IF EXISTS ${datas.name}`
                         client.db.run(liteRun)
                         client.db.run(`DELETE FROM ${datas.sender}sMessageBoxs WHERE id = '${datas.value}';`)
-                        client.fs.rmSync(`./page/users/${datas.sender}`,{recursive: true, force: true})
+                        client.fs.rmSync(`./page/users/${datas.sender}/${datas.name}.html`,{recursive: true, force: true})
                     }
                 });
               })
